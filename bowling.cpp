12cde98c6c8d0bfac3fa2bbb16cd68e775788e8f
prennushi <33952556+prennushi@users.noreply.github.com>
2017-11-24 10:06:04 +0100

    aggiunto arrey

--- bowling.cpp
+++ bowling.cpp
@@ -18,6 +18,12 @@ void SetColor(short Color)
 HANDLE hCon = GetStdHandle(STD_OUTPUT_HANDLE); 
 SetConsoleTextAttribute(hCon,Color); 
 }
-main(){int xp,yp,A[30],B[30],C[30],D[30],E[30];
+main(){int xp,yp,A[30],B[30],C[30],D[30],E[30],x,x2,y,y2;
  char H;
- 
+gotoxy(0;1);
+while(x<80){x++;
+            cout<<"=";}
+gotoxy(0;23);
+while(x<80){x++;
+            cout<<"*=";}
+      
